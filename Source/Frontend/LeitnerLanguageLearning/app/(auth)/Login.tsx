5ed10299b6bc977d54ead01e630b044b49a39c52
Zedster82 <Owenhr72@gmail.com>
2025-01-24 14:06:01 +0000

    Layout and Login

--- Source/Frontend/LeitnerLanguageLearning/app/(auth)/Login.tsx
+++ Source/Frontend/LeitnerLanguageLearning/app/(auth)/Login.tsx
@@ -1,33 +1,62 @@
-import { StyleSheet, Text, TextInput, View } from 'react-native'
-import React, { useEffect } from 'react'
+import { StyleSheet, Text, TextInput, View} from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { colors } from '@/constants/theme'
 import { useRouter } from 'expo-router'
 import ScreenWrapper from '@/components/ScreenWrapper'
 import Typo from '@/components/Typo'
 import Button from '@/components/Button'
-import { scale } from '@/utils/styling'
+import { horizontalScale } from '@/utils/styling'
 import Animated, { FadeIn } from 'react-native-reanimated'
+import { Input } from '@rneui/themed'
+import Auth from '@/components/Auth'
+// import { Session } from '@supabase/supabase-js'
+// import { supabase } from '@/utils/supabase'
+
 
 const Login = () => {
   const router = useRouter();
+  
+
+  const loginButton = () => {
+    console.log('Login Button Pressed');
+    router.push('/Homepage');
+  };
+
+  // const [session, setSession] = useState<Session | null>(null)
+
+  // useEffect(() => {
+  //   supabase.auth.getSession().then(({ data: { session } }) => {
+  //     setSession(session)
+  //   })
+
+  //   supabase.auth.onAuthStateChange((_event, session) => {
+  //     setSession(session)
+  //   })
+  // }, [])
+
+
 
   
 
+
+
   return (
-    <ScreenWrapper style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <View style={[styles.container, { height: '80%', width: '90%', borderRadius: 20, padding: 20, justifyContent: 'space-around', backgroundColor: colors.neutral800}]}>
+    <ScreenWrapper style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <View style={[styles.container, { height: '80%', width: '90%', borderRadius: 20, padding: 20, justifyContent: 'space-around', backgroundColor: colors.neutral800 }]}>
         <Typo color='aqua'>Login Page</Typo>
         <Typo size={12}>Please Login, or register if you do not have an account</Typo>
         <Animated.View
           entering={FadeIn.duration(1000)}
-          style={{height: 100, width: 100, backgroundColor: 'red'}}
+          style={{ height: 100, width: 100, backgroundColor: 'red' }}
         />
-        <TextInput placeholder="Username" />
-        <TextInput placeholder="Password" secureTextEntry={true} />
-        <Button onPress={() => router.push('/Homepage')}>
+
+        {/* <Auth /> */}
+        {/* <Typo>{session && session.user && <Text>{session.user.id}</Text>}</Typo> */}
+
+        <Button onPress={() => loginButton}>
           <Typo fontWeight={'400'}>Login</Typo>
         </Button>
-        <Button onPress={() => router.push('/(auth)/Register')}>
+        <Button onPress={() => router.push('/Playlists')}>
           <Typo fontWeight={'400'}>Register</Typo>
         </Button>
       </View>
--- Source/Frontend/LeitnerLanguageLearning/app/_layout.tsx
+++ Source/Frontend/LeitnerLanguageLearning/app/_layout.tsx
@@ -1,12 +1,23 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, useColorScheme, View } from 'react-native'
+
 import React from 'react'
 import { Stack } from 'expo-router'
+import { ThemeProvider, DarkTheme, DefaultTheme } from '@react-navigation/native'
 
 const _layout = () => {
-  return (
-    <Stack screenOptions={{headerShown: true}}>
 
-    </Stack>
+
+  const colorScheme = useColorScheme();
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack screenOptions={{ headerShown: false, }}>
+        <Stack.Screen name="index" options={{headerShown: true,}}/>
+
+        <Stack.Screen name="Homepage" options={{headerShown: true,}}/>
+
+
+      </Stack>
+    </ThemeProvider>
   )
 }
 
