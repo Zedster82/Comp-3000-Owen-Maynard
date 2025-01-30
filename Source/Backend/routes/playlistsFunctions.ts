dc931b7f8d84692757957419376a8653e2464c95
Zedster82 <Owenhr72@gmail.com>
2025-01-30 19:36:44 +0000

    Update swagger page

---
+++ Source/Backend/routes/playlistsFunctions.ts
@@ -0,0 +1,19 @@
+// Create playlist
+export const createPlaylist = async (req, res) => {
+    // ...implementation...
+}
+
+// Edit playlist
+export const editPlaylist = async (req, res) => {
+    // ...implementation...
+}
+
+// Delete playlist
+export const deletePlaylist = async (req, res) => {
+    // ...implementation...
+}
+
+// Get all playlists of user
+export const getAllPlaylists = async (req, res) => {
+    // ...implementation...
+}
